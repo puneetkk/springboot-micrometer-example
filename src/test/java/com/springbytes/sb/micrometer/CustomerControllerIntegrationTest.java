db8900eaf60e1fcdaa8f242d1d6bad03bff1ab3b
PuneetK <puneet.kankane@gmail.com>
2020-04-03 00:51:49 +0100

    DevSea-02 Updated comments

---
+++ src/test/java/com/springbytes/sb/micrometer/CustomerControllerIntegrationTest.java
@@ -0,0 +1,35 @@
+package com.springbytes.sb.micrometer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CustomerControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    /**
+     * Test case to check for basic setup and working of Get Operation
+     * @throws Exception
+     */
+    @Test
+    public void whenTestCustomerApp_thenAllResponse() throws Exception{
+        String result = "[{\"id\":1,\"version\":null,\"fname\":\"John\",\"lname\":\"Doe\"},{\"id\":2,\"version\":null,\"fname\":\"Stephen\",\"lname\":\"Smith\"},{\"id\":3,\"version\":null,\"fname\":\"John3\",\"lname\":\"Doe\"},{\"id\":4,\"version\":null,\"fname\":\"Stephen4\",\"lname\":\"Smith\"}]";
+        this.mockMvc.perform(get("/customer"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().string(result));
+    }
+}
